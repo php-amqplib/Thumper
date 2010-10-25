c882c9b8a2f2ddc231c47e69a4c81df32238c846
Alvaro Videla <videlalvaro@gmail.com>
2010-10-25 22:35:35 +0800

    fixes typo

--- examples/queue_server/consumer.php
+++ examples/queue_server/consumer.php
@@ -13,6 +13,6 @@ $consumer = new Consumer(HOST, PORT, USER, PASS, VHOST);
 $consumer->setExchangeOptions(array('name' => 'hello-exchange', 'type' => 'direct'));
 $consumer->setQueueOptions(array('name' => 'hello-queue'));
 $consumer->setCallback($myConsumer); //myConsumer could be any valid PHP callback
-$consumer->consume(5); //5 is the number of messages to consumes
+$consumer->consume(5); //5 is the number of messages to consume
 
 ?>
