28e515e90d261cdc7f3b447af43886b98987aac9
Alvaro Videla <videlalvaro@gmail.com>
2012-05-10 11:19:17 +0200

    prevents declaring the exchange for every published message

--- lib/Thumper/Producer.php
+++ lib/Thumper/Producer.php
@@ -6,10 +6,23 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class Producer extends BaseAmqp
 {
-  public function publish($msgBody, $routingKey = '')
-  {
-    $this->ch->exchange_declare($this->exchangeOptions['name'], $this->exchangeOptions['type'], false, true, false);
-    $msg = new AMQPMessage($msgBody, array('content_type' => 'text/plain', 'delivery_mode' => 2));
-    $this->ch->basic_publish($msg, $this->exchangeOptions['name'], $routingKey);
-  }
+    protected $exchangeReady = false;
+
+
+    public function __construct($host, $port, $user, $pass, $vhost)
+    {
+        parent::__construct($host, $port, $user, $pass, $vhost);
+    }
+
+    public function publish($msgBody, $routingKey = '')
+    {
+        if (!$this->exchangeReady) {
+            //declare a durable non autodelete exchange
+            $this->ch->exchange_declare($this->exchangeOptions['name'], $this->exchangeOptions['type'], false, true, false);
+            $this->exchangeReady = true;
+        }
+
+        $msg = new AMQPMessage($msgBody, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+        $this->ch->basic_publish($msg, $this->exchangeOptions['name'], $routingKey);
+    }
 }
